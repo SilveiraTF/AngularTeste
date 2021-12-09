4eaec174298efaf0455b2fa05545f08189385094
silveiraTF <silveiraforneli@gmail.com>
2021-12-09 11:01:19 -0300

    v2

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { map } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+//import { GitService } from './app.service';
+import { Git } from './app.model';
+import { FormControl, NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  git = {} as Git;
+  gits: Git[];
+
+  queryField = new FormControl();
+  url = 'https://api.github.com/users';
+  gits$: Observable<any>
+  id: number;
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {
+
+  }
+
+  getGit() {
+    let value = this.queryField.value
+
+    this.gits$= this.httpClient.get(this.url + '/' + value)
+    .pipe(
+      tap((data : any) => this.git = data))
+      map((res : any) => res.json().items)
+
+  }
+
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule, Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from'@angular/common/http';
+import { AppComponent } from './app.component';
+//import { GitService } from './app.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
